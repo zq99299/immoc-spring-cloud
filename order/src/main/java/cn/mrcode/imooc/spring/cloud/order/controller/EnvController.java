2dcbe2029c5fe76db522857dae90a9da6672af66
zhuqiang <99299684@qq.com>
2018-08-18 16:42:54 +0800

    bus 配置。 gitwebhook自动更新配置

--- order/src/main/java/cn/mrcode/imooc/spring/cloud/order/controller/EnvController.java
+++ order/src/main/java/cn/mrcode/imooc/spring/cloud/order/controller/EnvController.java
@@ -1,6 +1,7 @@
 package cn.mrcode.imooc.spring.cloud.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date : 2018/8/18 11:14
  */
 @RestController
+@RefreshScope
 public class EnvController {
     @Value("${env}")
     private String env;
