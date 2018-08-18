0bdb02a8e78e9d1e3169e578a84792d674a072db
zhuqiang <99299684@qq.com>
2018-08-18 14:33:06 +0800

    客户端配置中心初体验

---
+++ order/src/main/java/cn/mrcode/imooc/spring/cloud/order/controller/EnvController.java
@@ -0,0 +1,21 @@
+package cn.mrcode.imooc.spring.cloud.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : zhuqiang
+ * @version : V1.0
+ * @date : 2018/8/18 11:14
+ */
+@RestController
+public class EnvController {
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/env/print")
+    public String env() {
+        return env;
+    }
+}
