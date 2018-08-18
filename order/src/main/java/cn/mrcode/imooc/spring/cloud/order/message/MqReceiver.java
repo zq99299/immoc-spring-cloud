6ee60d6b72176efe3fbb060d25600db92e1ffebd
zhuqiang <99299684@qq.com>
2018-08-18 22:42:15 +0800

    去掉与配置中心关联，配置中心的已经讲完了。 后面开发连接配置中心有点麻烦；
    rabbitmq的基本使用。接收消息和发送消息

---
+++ order/src/main/java/cn/mrcode/imooc/spring/cloud/order/message/MqReceiver.java
@@ -0,0 +1,28 @@
+package cn.mrcode.imooc.spring.cloud.order.message;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * mq接收
+ * @author : zhuqiang
+ * @version : V1.0
+ * @date : 2018/8/18 22:21
+ */
+@Slf4j
+@Component
+public class MqReceiver {
+//    @RabbitListener(queues = "myQueue")
+//    //当队列不存在的时候，自动创建队列
+//    @RabbitListener(queuesToDeclare = @Queue("myQueue"))
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue("myQueue"), exchange = @Exchange("myExchange")
+    ))
+    public void process(String message) {
+        log.info("接收到消息 myQueue：{}", message);
+    }
+}
---
+++ order/src/test/java/cn/mrcode/imooc/spring/cloud/order/message/MqReceiverTest.java
@@ -0,0 +1,24 @@
+package cn.mrcode.imooc.spring.cloud.order.message;
+
+import cn.mrcode.imooc.spring.cloud.order.OrderApplicationTests;
+import org.junit.Test;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+/**
+ * ${todo}
+ * @author : zhuqiang
+ * @version : V1.0
+ * @date : 2018/8/18 22:23
+ */
+public class MqReceiverTest extends OrderApplicationTests {
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    public void send() {
+        amqpTemplate.convertAndSend("myQueue", new Date());
+    }
+}
