d4bbb867844a70778d03b0c14d2baacc46b0daa6
zhuqiang <99299684@qq.com>
2018-08-19 17:12:14 +0800

    服务网关和Zuul初体验

---
+++ api-gateway/src/main/java/cn/mrcode/imooc/spring/cloud/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package cn.mrcode.imooc.spring.cloud.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+}
